0906554c480bd422cff92a7a3437666766490067
Anubis Lockward <anubis.lockward@gmail.com>
2017-08-11 13:06:48 -0400

    Modified addFunds() and withdraw() methods on BankAccount.

--- src/com/lockward/BankAccount.java
+++ src/com/lockward/BankAccount.java
@@ -3,29 +3,78 @@ package com.lockward;
 public class BankAccount {
 
     private int accountNumber;
-    private int balance;
+    private double balance;
     private String customerName;
     private String email;
     private String phoneNumber;
 
-    public void addFunds(int amount) {
-        System.out.println("Adding: " + amount + " to account.");
-        balance += amount;
+    public BankAccount() {
+
     }
 
-    public int withdraw(int amount) {
-        if(amount > balance) {
-            System.out.println("Insufficient funds.");
-            return -1;
-        } else {
-            System.out.println("Withdrawing " + amount + " from account.");
-            balance -= amount;
+    public BankAccount(int accountNumber, double balance, String customerName, String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 
-            return amount;
+    public void addFunds(double amount) {
+        balance += amount;
+        System.out.println("Made a deposit of " + amount + " your new balance is " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if(balance - amount < 0) {
+            System.out.println("Not enough funds to widraw " + amount + " your balance is " + balance);
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawing " + amount + " from account, new balance is " + balance);
         }
     }
 
     public void checkBalance() {
         System.out.println("The account balance is " + balance);
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- src/com/lockward/Main.java
+++ src/com/lockward/Main.java
@@ -6,17 +6,9 @@ public class Main {
         BankAccount account = new BankAccount();
 
         account.checkBalance();
-
         account.addFunds(100);
-        account.checkBalance();
-
         account.addFunds(50);
-        account.checkBalance();
-
         account.withdraw(90);
-        account.checkBalance();
-
         account.withdraw(100);
-        account.checkBalance();
     }
 }
